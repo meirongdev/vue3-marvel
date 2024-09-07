6ab58616034a4e435d73df230e98129a4a878dfa
Meirong <meirongdev@gmail.com>
2024-09-08 00:14:58 +0800

    feat: adapt pwa by vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,49 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+  VitePWA({
+    registerType: "prompt",
+    injectRegister: false,
+    devOptions: {
+      enabled: true,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      // type: 'module',
+    },
+    workbox: {
+      globPatterns: ['**/*.{js, css, html, icon, png, svg, jpg}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+    includeAssets: ['fonts/*.ttf', 'images/*.png', 'css/*.css'],
+    manifest: {
+      short_name: 'Marvel Explorer',
+      name: "Mavel Explorer PWA",
+      description: "Marvel Explorer PWA for showcase",
+      // start_url: "/",
+      display: "standalone",
+      theme_color: "#333333",
+      background_color: "#000000",
+      orientation: "portrait",
+      icons: [
+        {
+          "src": "/android-chrome-192x192.png",
+          "sizes": "192x192", "type": "image/png"
+        },
+        {
+          "src": "/android-chrome-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        }],
+      prefer_related_applications: false,
+    },
+  })
+  ],
   resolve: {
     alias: [
       {
